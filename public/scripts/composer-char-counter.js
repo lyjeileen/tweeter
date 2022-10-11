6fbf5bbab5facbf9bde707da608f4e933e8bf467
lyjeileen <lyjeileen@gmail.com>
2022-10-11 23:58:59 +0000

    Add counter function

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("textarea").on("input", function (event) {
+    let count = 140 - $(this).val().length;
+    const counter = $(this).parent().find(".counter");
+    counter.text(count);
+    if (count < 0) {
+      counter.css("color", "red");
+    }
+  });
+});
