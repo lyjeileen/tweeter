82b776b61831ba16435c4fc81546d3407f2a42f8
lyjeileen <lyjeileen@gmail.com>
2022-10-12 21:46:14 +0000

    Add a JS function to generate tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,61 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+  const $tweets = $(".tweets-container");
+
+  const createTweetElement = function (tweet) {
+    let $tweet = $(`<article>
+            <header>
+              <div class="user">
+                <image src=${tweet.user.avatars} alt="avatar" height="40">
+                <div>${tweet.user.name}</div>
+              </div>
+              <div class="handle">${tweet.user.handle}</div>
+            </header>
+            <p>${tweet.content.text}</p>
+            <footer>
+              <div>${tweet.create_at}</div>
+              <div class="icon">
+                <i class="fa-solid fa-flag"></i>
+                <i class="fa-solid fa-retweet"></i>
+                <i class="fa-solid fa-heart"></i>
+              </div>
+            </footer>
+          </article>`);
+    return $tweet;
+  };
+
+  const renderTweets = function (tweets) {
+    tweets.forEach((tweet) => {
+      const $tweet = createTweetElement(tweet);
+      $tweets.append($tweet);
+    });
+  };
+  renderTweets(data);
+});
